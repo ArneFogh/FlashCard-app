588b822c9a147a40c410fe37f266235dd80371ba
arnefogh <andreasfogh7@gmail.com>
2023-04-14 12:08:17 +0200

    Set up rough the layout of our MainActivity(log in page), SignUpPage and ForgotPassword page

--- app/src/main/java/com/example/flashcardapp/SignUpPage.java
+++ app/src/main/java/com/example/flashcardapp/SignUpPage.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        getSupportActionBar().hide();
-
+        setContentView(R.layout.activity_sign_up_page);
     }
 }
