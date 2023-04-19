c22dac73179d12751fa54c257627e1958faf704f
arnefogh <andreasfogh7@gmail.com>
2023-04-19 15:57:43 +0200

    Now the flash cards are visible on the flash card home page

--- app/src/main/java/com/example/flashcardapp/FlashCardHome.java
+++ app/src/main/java/com/example/flashcardapp/FlashCardHome.java
@@ -1,35 +1,56 @@
 package com.example.flashcardapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.Date;
 
 public class FlashCardHome extends AppCompatActivity {
 
 
     FloatingActionButton mcreateFlashCardFab;
-
-
     private FirebaseAuth firebaseAuth;
+    RecyclerView mrecyclerView;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+    FirestoreRecyclerAdapter<firebaseModel, FlashCardViewHolder> flashCardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card_home);
 
-        getSupportActionBar().setTitle("All flash cards");
-
         mcreateFlashCardFab = findViewById(R.id.createFlashCardFab);
         firebaseAuth = FirebaseAuth.getInstance();
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getSupportActionBar().setTitle("All flash cards");
         mcreateFlashCardFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,8 +58,54 @@ public class FlashCardHome extends AppCompatActivity {
             }
         });
 
+        Query query = firebaseFirestore.collection("flashCards").document(firebaseUser.getUid()).collection("myFlashCards").orderBy("title", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<firebaseModel> allUserFlashCards = new FirestoreRecyclerOptions.Builder<firebaseModel>().setQuery(query, firebaseModel.class).build();
+
+        flashCardAdapter = new FirestoreRecyclerAdapter<firebaseModel, FlashCardViewHolder>(allUserFlashCards) {
+
+            @Override
+            protected void onBindViewHolder(@NonNull FlashCardViewHolder flashCardViewHolder, int i, @NonNull firebaseModel firebaseModel) {
 
 
+                flashCardViewHolder.flashCardTitle.setText(firebaseModel.getTitle());
+                //flashCardViewHolder.flashCardAnswer.setText(firebaseModel.getAnswer());
+                //flashCardViewHolder.flashCardQuestion.setText(firebaseModel.getQuestion());
+
+            }
+
+
+
+            @NonNull
+            @Override
+            public FlashCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.flash_card_layout, parent, false);
+                return new FlashCardViewHolder(view);
+            }
+        };
+
+        mrecyclerView = findViewById(R.id.recyclerView);
+        mrecyclerView.setHasFixedSize(true);
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        mrecyclerView.setLayoutManager(staggeredGridLayoutManager);
+        mrecyclerView.setAdapter(flashCardAdapter);
+
+
+    }
+
+
+    public class FlashCardViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView flashCardTitle;
+        private TextView flashCardAnswer;
+        private TextView flashCardQuestion;
+        LinearLayout mflashCard;
+
+        public FlashCardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            flashCardTitle = itemView.findViewById(R.id.flashCardTitle);
+            mflashCard = itemView.findViewById(R.id.flashCard);
+
+        }
     }
 
 
@@ -60,5 +127,20 @@ public class FlashCardHome extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        flashCardAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (flashCardAdapter != null){
+            flashCardAdapter.stopListening();
+        }
     }
 }
